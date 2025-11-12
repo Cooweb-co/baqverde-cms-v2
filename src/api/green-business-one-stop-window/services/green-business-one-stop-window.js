2bfe336c3d7e38c2ce506923d5856f81e8fd2729
Brandon Corpas <corpasdev@gmail.com>
2025-11-11 20:21:47 -0500

    feat: add Green Business Page management

---
+++ src/api/green-business-one-stop-window/services/green-business-one-stop-window.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::green-business-one-stop-window.green-business-one-stop-window');
